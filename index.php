dfb072811543e2aae068d55c64090b7c226d611e
tahirmirji <tahiritguru@gmail.com>
2023-12-28 16:52:39 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <form action="" method="post">
+  <div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col-md-2">
+      
+    </div>
+    <div class="col-md-8 bg-info p-5 m-5" >
+    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Name</label>
+  <input type="email" class="form-control" name="name" id="exampleFormControlInput1" placeholder="name">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email</label>
+  <input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="Email">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Contact</label>
+  <input type="email" class="form-control" name="contact" id="exampleFormControlInput1" placeholder="contact">
+</div>
+<button type="submit" class="btn btn-success">Submit</button>
+
+
+    </div>
+    <div class="col-md-2">
+      
+    </div>
+  </div>
+</div>
+    
+</form>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
